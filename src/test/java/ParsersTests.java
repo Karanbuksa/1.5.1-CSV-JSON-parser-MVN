39470495890ed9ceea4414c9ef4dccc903d00baa
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2022-12-19 20:29:59 +0500

    Tests with hamcrest

--- src/test/java/ParsersTests.java
+++ src/test/java/ParsersTests.java
@@ -1,30 +1,38 @@
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ParsersTests {
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class ParsersTests {
+    List<Employee> expectedEmployees = new ArrayList<>();
 
     /**
      * ѕыталс€ примен€ть аннотации @BeforeAll и @BeforeEach дл€ заполнени€ листа сотрудников объектами,
      * но в итоге в тесты приходили пустые листы
      **/
+@BeforeEach
+public void start(){
+    expectedEmployees.add(new Employee(1, "John", "Smith", "USA", 25));
+    expectedEmployees.add(new Employee(2, "Ivan", "Petrov", "RU", 23));
+    expectedEmployees.add(new Employee(3, "David", "Tenant", "UK", 45));
+    expectedEmployees.add(new Employee(4, "Math", "Smith", "UK", 38));
+}
 
     @Test
     public void parseXML_test() throws ParserConfigurationException, IOException, SAXException {
         String data = "src/test/resources/data.xml";
-        List<Employee> expectedEmployees = new ArrayList<>();
-        expectedEmployees.add(new Employee(1, "John", "Smith", "USA", 25));
-        expectedEmployees.add(new Employee(2, "Ivan", "Petrov", "RU", 23));
-        expectedEmployees.add(new Employee(3, "David", "Tenant", "UK", 45));
-        expectedEmployees.add(new Employee(4, "Math", "Smith", "UK", 38));
+
         List<Employee> employees = Main.parseXML(data);
 
-        Assertions.assertEquals(expectedEmployees.toString(), employees.toString());
+        assertThat(expectedEmployees.toString(), equalTo(employees.toString()));
 
     }
 
@@ -40,7 +48,7 @@ public class ParsersTests {
 
         List<Employee> employees = Main.parseCSV(columnMapping, data);
 
-        Assertions.assertEquals(expectedEmployees.toString(), employees.toString());
+        assertThat(employees.toString(), equalTo(expectedEmployees.toString()));
 
 
     }
@@ -56,8 +64,7 @@ public class ParsersTests {
 
         List<Employee> employees = Main.jsonToList(Main.readString(data));
 
-        Assertions.assertEquals(expectedEmployees.toString(), employees.toString());
-
+        assertThat(employees.toString(), equalTo(expectedEmployees.toString()));
 
     }
 }
